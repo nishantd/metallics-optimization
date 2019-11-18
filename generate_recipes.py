628c70ae16a13e98d1b62e69b3dee43a150248a1
Nishant Deshpande <nishant@myralabs.com>
2019-11-18 13:11:33 -0800

    small fixes and a hack to be fixed later

--- generate_recipes.py
+++ generate_recipes.py
@@ -4,6 +4,7 @@ import json
 
 import copper_model
 import yield_model
+import cost_estimator
 import recipe_optimizer
 
 logging.basicConfig()
@@ -24,8 +25,12 @@ def main():
     y_model = yield_model.YieldModel(data_dir)
     y_model.create_model()
 
+    cost_es = cost_estimator.CostEstimator(data_dir)
+    cost_es.initialize()
+
     ro = recipe_optimizer.RecipeOptimizer(
-        data_dir=data_dir, copper_model=cu_model, yield_model=yield_model)
+        data_dir=data_dir, copper_model=cu_model, yield_model=yield_model,
+        cost_estimator=cost_es)
     ro.initialize()
     schedule_recipes = ro.create_recipes_for_schedule()
 
--- recipe_optimizer.py
+++ recipe_optimizer.py
@@ -2,10 +2,11 @@ import json
 import sys, os
 
 class RecipeOptimizer(object):
-    def __init__(self, data_dir, copper_model, yield_model):
+    def __init__(self, data_dir, copper_model, yield_model, cost_estimator):
         self.data_dir = data_dir
         self.copper_model = copper_model
         self.yield_model = yield_model
+        self.cost_estimator = cost_estimator
 
     def initialize(self):
         # Uses the data to initialize if required.
@@ -13,8 +14,11 @@ class RecipeOptimizer(object):
 
     def create_recipes_for_schedule(self):
         # TODO: Mock for now.
+        # Just return back the example final output.
         s = None
-        with open(os.path.dirname(self.data_dir.rsplit('/', 1)[0]) + '/final_output_example.json') as f:
+        #with open(os.path.join(self.data_dir.rsplit('/', 1)[0], '/final_output_example.json')) as f:
+        # TODO: fix above
+        with open('data/final_output_example.json') as f:
             s = f.read()
         schedule_recipes = json.loads(s)
         return schedule_recipes
